a89ae6bfab0e406892a36329e732cae3b1625900
Chris <chriswilliampeterson@gmail.com>
2022-07-23 16:12:43 -0600

    adding answers to Q2, 3, 4

--- Unit 8 - SQL & Databases/8.3 SQL Case Study - Country Club/SQLFiles Tier 1/SOLUTION_SQLTasks Tier 1.sql
+++ Unit 8 - SQL & Databases/8.3 SQL Case Study - Country Club/SQLFiles Tier 1/SOLUTION_SQLTasks Tier 1.sql
@@ -41,15 +41,30 @@ ORDER BY membercost DESC
 
 /* Q2: How many facilities do not charge a fee to members? */
 
+SELECT COUNT(*)
+FROM Facilities
+WHERE membercost = 0
+
+4 - Badminton, table tennis, snicker, pool
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
+SELECT facid, name, membercost, monthlymaintenance
+FROM Facilities
+WHERE membercost > 0
+AND membercost / monthlymaintenance < 0.2
+
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
+
+SELECT *
+FROM Facilities
+WHERE facid
+IN(1, 5)
 
 
 /* Q5: Produce a list of facilities, with each labelled as
